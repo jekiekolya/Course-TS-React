b336185c6a579f796d0e7a4e2805ece1f8536fd4
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-13 18:47:57 +0200

    Update asyncTodoSlice.ts

--- src/redux/todo/asyncTodoSlice.ts
+++ src/redux/todo/asyncTodoSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import todoActions from "./todoOperations";
 
 import { Todo } from "types";
 
@@ -16,6 +17,32 @@ const todoSlice = createSlice({
   name: "todos",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // Get all todos
+      .addCase(todoActions.fetchAllTodos.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(todoActions.fetchAllTodos.fulfilled, (state, action) => {
+        state.status = "finished";
+        state.todos = action.payload;
+      })
+      .addCase(todoActions.fetchAllTodos.rejected, (state) => {
+        state.status = "error";
+      })
+
+      // Create
+      .addCase(todoActions.createTodo.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(todoActions.createTodo.fulfilled, (state, action) => {
+        state.status = "finished";
+        state.todos.push(action.payload);
+      })
+      .addCase(todoActions.createTodo.rejected, (state) => {
+        state.status = "error";
+      });
+  },
 });
 
 export default todoSlice.reducer;
